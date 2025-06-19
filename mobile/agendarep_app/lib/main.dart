b6963784c5de86297c2375ca1703ddbac65a54f1
albertothe <albertothe@gmail.com>
2025-06-19 17:04:54 -0300

    Initialize date formatting for pt_BR locale

--- mobile/agendarep_app/lib/main.dart
+++ mobile/agendarep_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'home_page.dart';
 import 'login_page.dart';
 import 'settings_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR', null);
   runApp(const AgendaRepApp());
 }
 
