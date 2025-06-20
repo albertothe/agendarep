da9f9a5dd71cb48929170d5630eb7d9caca964c6
Alberto Teixeira <albertothe@gmail.com>
2025-06-20 07:46:58 -0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     apps: [
         {
-            name: 'b3b-backend',
+            name: 'agendarep-backend',
             cwd: './backend',
             script: 'node_modules/ts-node/dist/bin.js',
             args: 'src/index.ts',
@@ -11,11 +11,11 @@ module.exports = {
             }
         },
         {
-            name: 'b3b-frontend',
+            name: 'agendarep-frontend',
             cwd: './frontend',
             interpreter: 'node',
             script: '../node_modules/serve/build/main.js',
-            args: ['-s', 'dist', '-l', '9500', '--single'],
+            args: ['-s', 'dist', '-l', '8500', '--single'],
             env: {
                 NODE_ENV: 'production'
             }
